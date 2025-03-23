2e437baba247f3111982f681d571730739bda60f
vaibhav <vs8631304@gmail.com>
2025-03-23 14:33:51 +0530

    added github route

--- index.js
+++ index.js
@@ -2,6 +2,29 @@ const express = require('express');
 require('dotenv').config();
 const app = express();
 
+
+const githubData = {
+    "login": "mojombo",
+    "id": 1,
+    "node_id": "MDQ6VXNlcjE=",
+    "avatar_url": "https://avatars.githubusercontent.com/u/1?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/mojombo",
+    "html_url": "https://github.com/mojombo",
+    "followers_url": "https://api.github.com/users/mojombo/followers",
+    "following_url": "https://api.github.com/users/mojombo/following{/other_user}",
+    "gists_url": "https://api.github.com/users/mojombo/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/mojombo/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/mojombo/subscriptions",
+    "organizations_url": "https://api.github.com/users/mojombo/orgs",
+    "repos_url": "https://api.github.com/users/mojombo/repos",
+    "events_url": "https://api.github.com/users/mojombo/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/mojombo/received_events",
+    "type": "User",
+    "user_view_type": "public",
+    "site_admin": false
+    }
+
 app.get("/", (req, res) => {
     res.send("Hello World!");
 })
@@ -16,6 +39,10 @@ app.get("/login",(req,res)=>{
 
 app.get("/youtube",(req,res)=>{
     res.send("<h1>Chai aur code </h1>");
+})
+
+app.get("/github",(req,res)=>{
+    res.json(githubData);
 })
 app.listen(process.env.PORT, () => {
     console.log(`Example app listening at http://localhost:${process.env.PORT}`);
